dcda4779153110a37ffbbea45e4501283162a750
Mahad <mahadmunir10@gmail.com>
2022-05-17 17:06:00 +0500

    change db credentials

--- DjangoTest/settings/production.py
+++ DjangoTest/settings/production.py
@@ -15,9 +15,9 @@ if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
         'default': {
            'ENGINE': 'django.db.backends.postgresql',
-           'NAME': 'github_actions',
-           'USER': 'radorado',
-           'PASSWORD': 'radorado',
+           'NAME': os.environ['POSTGRES_DB'],
+           'USER': os.environ['POSTGRES_USER'],
+           'PASSWORD': os.environ['POSTGRES_PASSWORD'],
            'HOST': '127.0.0.1',
            'PORT': '5432',
         }
