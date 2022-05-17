87ce992cea20d96690a17e0ff9911604f5306c38
Mahad <mahadmunir10@gmail.com>
2022-05-17 17:53:52 +0500

    add db host & pass

--- DjangoTest/settings/production.py
+++ DjangoTest/settings/production.py
@@ -2,23 +2,11 @@ import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'github_actions',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': os.environ['POSTGRES_DB'],
+       'USER': os.environ['POSTGRES_USER'],
+       'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+       'HOST': os.environ['HOST'],
+       'PORT': os.environ['PORT'],
     }
 }
-
-if os.environ.get('GITHUB_WORKFLOW'):
-    DATABASES = {
-        'default': {
-           'ENGINE': 'django.db.backends.postgresql',
-           'NAME': os.environ['POSTGRES_DB'],
-           'USER': os.environ['POSTGRES_USER'],
-           'PASSWORD': os.environ['POSTGRES_PASSWORD'],
-           'HOST': '127.0.0.1',
-           'PORT': '5432',
-        }
-    }
